ef14015bb935769885b5d4c007202932ccd8bed5
BiggerOofMan <87370784+BiggerOofMan@users.noreply.github.com>
2025-11-17 16:10:01 -0500

    Update calculator.py
    Added basic arithmetic functions: add, sub, mul, div, log, exp.

--- calculator.py
+++ calculator.py
@@ -10,6 +10,23 @@ One function per operation, in order.
 """
 import math
 
+def add(a, b): 
+    pass
+
+def sub(a, b): 
+    pass
+    
+def mul(a, b): 
+    pass
+    
+def div(a, b): 
+    pass
+    
+def log(a, b): 
+    pass
+    
+def exp(a, b): 
+    pass
 
 def square_root(a):
     if a < 0:
