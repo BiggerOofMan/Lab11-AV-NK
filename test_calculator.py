4dc6fa89368c43716d69299ce415ebc0f5d24036
BiggerOofMan <87370784+BiggerOofMan@users.noreply.github.com>
2025-11-17 16:05:14 -0500

    Update test_calculator.py

--- test_calculator.py
+++ test_calculator.py
@@ -2,11 +2,35 @@
 # Partner 1: Anirudh S. Vijayaraman
 # Partner 2: Dropping the Lab
 
-
 import unittest
 from calculator import *
 
+
 class TestCalculator(unittest.TestCase):
+    def test_multiply(self):
+        self.assertEqual(multiply(3, 4), 12)
+        self.assertEqual(multiply(-2, 5), -10)
+
+    def test_divide(self):
+        self.assertEqual(divide(2, 10), 5)
+        with self.assertRaises(ZeroDivisionError):
+            divide(0, 5)
+
+    def test_log_invalid_argument(self):
+        with self.assertRaises(ValueError):
+            logarithm(1, 10)
+        with self.assertRaises(ValueError):
+            logarithm(2, -5)
+
+    def test_hypotenuse(self):
+        self.assertAlmostEqual(hypotenuse(3, 4), 5)
+        self.assertAlmostEqual(hypotenuse(-3, 4), 5)
+
+    def test_sqrt(self):
+        self.assertAlmostEqual(square_root(9), 3)
+        with self.assertRaises(ValueError):
+            square_root(-1)
+
 
 if __name__ == "__main__":
     unittest.main()
